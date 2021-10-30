90eabc9808196b5413ca46ec1228c6b9faa18826
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-30 13:11:34 -0400

    create destination form body and page

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,9 +1,21 @@
 import { Destination } from '../models/destination.js'
 
+
 function newDestination(req, res) {
-  res.render('destinations/new')
+  Destination.find({}, function(error, destinations) {
+    res.render('destinations/new', {
+      destinations
+    })
+  })
+}
+
+function create(req, res) {
+  Destination.create(req.body, function(error, destination) {
+    res.redirect('/destinations/new')
+  })
 }
 
 export {
-  newDestination as new
+  newDestination as new,
+  create,
 }
--- routes/destinations.js
+++ routes/destinations.js
@@ -2,8 +2,9 @@ import { Router } from 'express'
 import * as destinationsCtrl from "../controllers/destinations.js";
 const router = Router()
 
+
 router.get('/new', destinationsCtrl.new)
-// router.post('')
+router.post('/', destinationsCtrl.create)
 
 export {
   router
